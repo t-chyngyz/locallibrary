c6147c0a40de7747613e2d3d9cf58bb868f7274b
Chyngyz Toichubekov <t.chyngyz@yandex.ru>
2021-03-14 16:22:09 +0600

    14.03.2021

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,16 +8,24 @@ admin.site.register(Genre)
 # admin.site.register(BookInstance)
 admin.site.register(Language)
 
+class BookInline(admin.TabularInline):
+    model = Book
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 
 @admin.register(BookInstance)
