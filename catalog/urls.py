41045f1a9a395cf9590529961c3ca9824e8835d4
Chyngyz Toichubekov <t.chyngyz@yandex.ru>
2021-03-14 16:34:10 +0600

    14.03.2021_v2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 
-urlpatterns += [
-     path('catalog/', include('catalog.urls')),
-]
+# urlpatterns += [
+#      path('catalog/', include('catalog.urls')),
+# ]
 from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
